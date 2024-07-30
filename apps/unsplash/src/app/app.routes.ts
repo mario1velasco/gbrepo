efeec4a960f76416cb22ab3e1fdd6645a5a81e0a
mariovelascoalonso <mario.velasco@bizaway.com>
2024-07-30 17:41:11 +0200

    Create a basic routing

--- apps/unsplash/src/app/app.routes.ts
+++ apps/unsplash/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: DashboardComponent },
+  {
+    loadChildren: () =>
+      import('./pages/images/images.module').then((m) => m.ImagesModule),
+    path: 'images',
+  },
+  { path: '**', component: NotFoundComponent },
+];
