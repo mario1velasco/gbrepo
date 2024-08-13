958baf2b21bf30a0b5b9e83d0df3eea0a6a1bfff
mariovelascoalonso <mario.velasco@bizaway.com>
2024-08-13 10:40:36 +0200

    Feat: ImagesListFilterComponent finish with design and toggles

--- apps/unsplash/src/app/pages/images/images-list/components/images-list-filter/images-list-filter.component.ts
+++ apps/unsplash/src/app/pages/images/images-list/components/images-list-filter/images-list-filter.component.ts
@@ -1,11 +1,18 @@
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ImageFormType } from '../../../shared/image.types';
+import { ButtonComponent, DropdownComponent } from '@gbrepo/ui';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgIf, ReactiveFormsModule],
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+    DropdownComponent,
+    ButtonComponent,
+    NgClass,
+  ],
   selector: 'app-images-list-filter',
   standalone: true,
   templateUrl: './images-list-filter.component.html',
@@ -14,4 +21,22 @@ import { ImageFormType } from '../../../shared/image.types';
 export class ImagesListFilterComponent {
   // * Inputs
   @Input() public form: ImageFormType | undefined;
+
+  // * Variables
+  public isMaximized = false;
+  public isFormVisible = false;
+
+  // *******************
+  // * EVENTS
+  // *******************
+  onSubmit() {
+    console.log(this.form?.value);
+  }
+
+  toggleMaximize() {
+    this.isMaximized = !this.isMaximized;
+  }
+  toggleFormVisibility() {
+    this.isFormVisible = !this.isFormVisible;
+  }
 }
